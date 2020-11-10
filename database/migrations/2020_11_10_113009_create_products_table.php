d285b3add3b1da940752eb7e1873c8245cf1f2f8
Zihad <zihadnayem8@gmail.com>
2020-11-10 11:40:42 +0600

    Added Product Model, Controller and  Migration

---
+++ database/migrations/2020_11_10_113009_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('title', 128)->unique();
+            $table->string('slug', 160)->unique();
+            $table->longText('description');
+            $table->boolean('in_stock')->default(true);
+            $table->decimal('price', 8, 2);
+            $table->decimal('sales_price', 8, 2)->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
