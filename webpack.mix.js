3232c7a6d234cd9fbf4876c97ff4dc7678a752de
Zihad <zihadnayem8@gmail.com>
2020-11-25 14:02:55 +0600

    updated npm and install bootstarp4

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+ mix.sass('resources/sass/app.scss', 'public/css')
+    .styles(['resources/css/custom.css'], 'public/css/custom.css')
+    .scripts(['resources/js/app.js','resources/js/custom.js' ], 'public/js/app.js').version();
