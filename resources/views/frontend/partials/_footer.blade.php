51599fce3918912bb234d5fddb127ebd810db3cb
Zihad <zihadnayem8@gmail.com>
2020-12-06 12:00:19 +0600

    master layout done.

---
+++ resources/views/frontend/partials/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">Back to top</a>
+      </p>
+      <p>&copy; {{ config('app.name') }} {{ date('Y') }}</p>
+      <p>Read our? <a href="#">Privacy policy</a> or <a href="#">Contact us</a>.</p>
+    </div>
+  </footer>
